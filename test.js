7c118bfa548e7c711501b35601a62bb6305ae9bf
Danny Funk <danny.funk@ni.com>
2016-11-30 09:44:07 -0600

    added parameter on function
    takes color in

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ var colors = require('colors');
 
 var output = lodash.without([1,2,3,4],1);
 console.log(output);
-exports.printMsg = function(){
-    console.log("A test log from Danny" .red);
+exports.printMsg = function(color){
+    console.log("A test log from Danny" [color]);
 };
